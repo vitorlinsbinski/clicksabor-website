07b930048ca3342faf3ac33ac5011530ef3a34f2
Vitor Linsbinski de Oliveira <vitor.linsbinski@hotmail.com>
2025-06-25 14:43:13 -0400

    adicionar: sidebar carrinho

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 const navItem = document.getElementById('menu');
 const burgerIconItem = document.getElementById('burgerIcon');
-const closeIconItem = document.getElementById('closeIcon');
+const btnCloseMenu = document.getElementById('btnCloseMenu');
 const cardapioNavItem = document.getElementById('cardapio-navitem');
 const dropdownItem = document.getElementById('dropdown');
 const itensMenu = document.getElementsByClassName('item-menu');
 const overlayItem = document.getElementById('overlay');
+const cartOpenBtn = document.getElementById('cartBtn');
+const cartCloseBtn = document.getElementById('btnCloseCart');
+const cartSidebar = document.getElementById('cartSidebar');
 
 burgerIconItem.addEventListener('click', () => {
   navItem.classList.toggle('active');
+  overlayItem.classList.add('active');
 });
 
-closeIconItem.addEventListener('click', () => {
+btnCloseMenu.addEventListener('click', () => {
   navItem.classList.toggle('active');
+  overlayItem.classList.remove('active');
 });
 
 overlayItem.addEventListener('click', () => {
-  navItem.classList.toggle('active');
+  navItem.classList.remove('active');
+  overlayItem.classList.remove('active');
+  cartSidebar.classList.remove('active');
 });
 
 cardapioNavItem.addEventListener('click', () => {
@@ -25,5 +32,44 @@ cardapioNavItem.addEventListener('click', () => {
 Array.from(itensMenu).forEach((item) => {
   item.addEventListener('click', () => {
     navItem.classList.toggle('active');
+    overlayItem.classList.remove('active');
   });
 });
+
+cartOpenBtn.addEventListener('click', () => {
+  cartSidebar.classList.toggle('active');
+  overlayItem.classList.add('active');
+});
+
+cartCloseBtn.addEventListener('click', () => {
+  cartSidebar.classList.toggle('active');
+  overlayItem.classList.remove('active');
+});
+
+const products = [
+  {
+    id: 1,
+    title: 'Feijoada',
+    description: 'Tradicional prato brasileiro feito com feijão preto',
+    imgPath: './assets/img-feijoada.png',
+  },
+
+  {
+    id: 2,
+    title: 'Feijoada',
+    description: 'Tradicional prato brasileiro feito com feijão preto',
+    imgPath: './assets/img-feijoada.png',
+  },
+  {
+    id: 3,
+    title: 'Feijoada',
+    description: 'Tradicional prato brasileiro feito com feijão preto',
+    imgPath: './assets/img-feijoada.png',
+  },
+  {
+    id: 4,
+    title: 'Feijoada',
+    description: 'Tradicional prato brasileiro feito com feijão preto',
+    imgPath: './assets/img-feijoada.png',
+  },
+];
